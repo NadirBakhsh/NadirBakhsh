dd8f0bf79eb1eae241dc579b8aa533eef91a96ec
NadirBakhsh <nadir4k2010@gmail.com>
2025-01-31 19:09:52 +0500

    Section added

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,17 +1,46 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node'
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
-
-export default function Index() {
-  return (
-   <div>
-   </div>
-  );
+	return [
+		{ title: 'New Remix App' },
+		{ name: 'description', content: 'Welcome to Remix!' },
+	]
 }
 
+export default function Index() {
+	return (
+		<div>
+			{/* Hero Section start */}
+			<div className="hero h-screen bg-white">Hero section</div>
+			{/* Hero Section end */}
 
+			{/* Skills Section start */}
+			<div className="skills h-screen bg-white">Skills section</div>
+			{/* Skills Section end */}
+
+			{/* Experience Section start */}
+			<div className="Experience h-screen bg-black">Experience section</div>
+			{/* Experience Section end */}
+
+			{/* About Section start */}
+			<div className="About h-screen bg-white">About section</div>
+			{/* About Section end */}
+
+			{/* Projects Section start */}
+			<div className="Projects h-screen bg-black">Projects section</div>
+			{/* Projects Section end */}
+
+			{/* Testimonial Section start */}
+			<div className="Testimonial h-screen bg-white">Testimonial section</div>
+			{/* Testimonial Section end */}
+
+			{/* Contact Section start */}
+			<div className="Contact h-screen bg-white">Contact section</div>
+			{/* Contact Section end */}
+
+			{/*footer Section start */}
+			<div className="footer h-[100px] w-full bg-black">footer section</div>
+			{/* footer Section end */}
+		</div>
+	)
+}
