9d9ebd207e69638eba91e6615732fde41851ef76
NadirBakhsh <nadir4k2010@gmail.com>
2025-02-14 00:02:21 +0500

    expr responsive

--- app/components/sections/ExperienceSection.tsx
+++ app/components/sections/ExperienceSection.tsx
@@ -6,7 +6,7 @@ function ExperienceSection(props: Props) {
   const {} = props
 
   return (
-    <div className="Experience h-auto bg-black mt-6 pb-20">
+    <div className="Experience h-auto bg-black mt-6 pb-20 px-5 md:px-0">
       <div className="container mx-auto ">
         <h2 className="text-5xl text-center text-white py-20">
           {' '}
@@ -16,15 +16,15 @@ function ExperienceSection(props: Props) {
         {Array.from({ length: 3 }).map((_, i) => (
           <div
             key={i}
-            className="text-white border-2 border-zinc-800 rounded-md p-6  hover:bg-zinc-800 mb-8">
-            <div className="card-hear flex justify-between">
-              <div className="space-x-5 font-semibold">
-                <span>G</span>
+            className="text-white border-2 border-zinc-500 rounded-lg p-6  hover:bg-zinc-800 mb-8">
+            <div className="card-hear flex flex-col md:flex-row justify-between md:items-end">
+              <div className="space-x-4 font-semibold flex  md:items-end">
+                <span className='w-10 h-10 bg-white shrink-0 grow-0 '>G</span>
                 <span className="text-xl">
                   Lead Software Engineer at Google {i + 1}
                 </span>
               </div>
-              <span className="text-base">Nov {2019 + i} - Present</span>
+              <span className="text-base mt-[30px] md:mt-0">Nov {2019 + i} - Present</span>
             </div>
             <p className="text-base mt-4 !font-light text-zinc-400">
               As a Senior Software Engineer at Google, I played a pivotal role
