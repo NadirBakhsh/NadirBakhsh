8e3a0e4564ee55754b46bafc6e68ceacc4bf66dc
NadirBakhsh <nadir4k2010@gmail.com>
2025-02-14 00:33:21 +0500

    responsive project section

--- app/components/sections/ProjectsSection.tsx
+++ app/components/sections/ProjectsSection.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 
 interface Props {}
 
@@ -6,24 +5,25 @@ function ProjectsSection(props: Props) {
   const {} = props
 
   return (
-    <div className="Projects bg-black py-10 ">
+    <div className="Projects bg-black py-10 px-5 md:px-0">
       <div className="container mx-auto py-5">
         <h2 className="text-5xl text-center text-white ">
-          {' '}
+          {" "}
           My <span className="font-extrabold text-center">Projects</span>
         </h2>
 
         {Array.from({ length: 3 }).map((_, i) => (
           <div
             key={i}
-            className="project card flex mt-16 h-[516px] gap-10 odd:flex-row-reverse relative">
-            <div className="flex-1 bg-red-200 rounded-2xl"></div>
+            className="project card flex mt-10 md:mt-16 gap-10 flex-col md:flex-row odd:md:flex-row-reverse relative border-b-2 pb-8 md:pb-0 border-zinc-400 md:border-0"
+          >
+            <div className="md:flex-1 bg-red-200 rounded-2xl shrink-0 grow-0 h-[400px] md:h-auto"></div>
             <div className="flex-1">
               <h2 className="text-6xl font-bold text-white">{`0${i + 1}`}</h2>
-              <h3 className="text-white text-3xl font-semibold my-5">
+              <h3 className="text-white text-xl md:text-[32px] font-semibold my-4 md:my-5">
                 Project Name
               </h3>
-              <p className="text-zinc-400 text-base mt-3">
+              <p className="text-zinc-400 text-sm md:text-base md:mt-3">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 Dignissimos quae pariatur modi, minima ipsum facilis et
                 perspiciatis Lorem ipsum dolor sit amet consectetur adipisicing
@@ -38,13 +38,14 @@ function ProjectsSection(props: Props) {
                 elit. Dignissimos quae pariatur modi, minima ipsum facilis et
                 perspiciatis
               </p>
-              <div className=" mt-5">
-                <h5 className="text-zinc-200 text-xl">Tech Stack</h5>
+              <div className="mt-5">
+                <h5 className="text-zinc-200 text-lg md:text-xl">Tech Stack</h5>
                 <div className="flex gap-3">
                   {Array.from({ length: 4 }).map((_, i) => (
                     <div
                       key={i}
-                      className="h-10 mt-2 w-10 border-zinc-400 hover:bg-primary-neutral  border-2 rounded-md flex items-center justify-center"></div>
+                      className="h-10 mt-2 w-10 border-zinc-400 hover:bg-primary-neutral  border-2 rounded-md flex items-center justify-center"
+                    ></div>
                   ))}
                 </div>
               </div>
