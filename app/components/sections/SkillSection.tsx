18525cea405549a35c15837e53c01508d9193751
NadirBakhsh <nadir4k2010@gmail.com>
2025-02-13 23:43:50 +0500

    skill responsive

--- app/components/sections/SkillSection.tsx
+++ app/components/sections/SkillSection.tsx
@@ -13,11 +13,11 @@ function SkillSection(props: Props) {
           My <span className="font-extrabold">Skills</span>
         </h2>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-10 my-10">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-5 gap-6 md:gap-10 my-10">
           {Array.from({ length: 10 }).map((_, i) => (
             <div
               key={i}
-              className="h-[186px] text-black hover:text-white hover:bg-black text-xl font-semibold w-[186px] border-2 border-black rounded-md flex items-center justify-center gap-y-8">
+              className="h-[160px] md:h-[186px] text-black hover:text-white hover:bg-black text-xl font-semibold w-[161px] md:w-[186px] border-2 border-black rounded-md flex items-center justify-center gap-y-8">
               {i}
             </div>
           ))}
