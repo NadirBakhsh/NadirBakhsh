6e162bbedd07743f29c93f1f58329446eca02a59
NadirBakhsh <nadir4k2010@gmail.com>
2025-02-22 00:13:00 +0500

    typescript happy

--- app/components/sections/SkillSection.tsx
+++ app/components/sections/SkillSection.tsx
@@ -1,4 +1,4 @@
-import portfolioJson from "~/json/portfolio"
+import portfolioJson from "~/json/portfolio.json"
 import { Icons } from "../icon"
 
 interface Props {}
@@ -19,18 +19,18 @@ function SkillSection(props: Props) {
         </h2>
 
         <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-5 gap-6 md:gap-10 my-12">
-          {skills.map((item: any) => {
-            const SkillIcon = Icons[item?.icon]
+          {skills.map((item) => {
+            const SkillIcon = Icons[item.icon as keyof typeof Icons]
 
             return (
               <div
-                key={item?.name}
+                key={item.name}
                 className="h-[160px] p-4 group cursor-pointer flex-col md:h-[186px] text-black hover:text-white hover:bg-black text-xl font-semibold w-[161px] md:w-[186px] border-2 border-black rounded-md flex items-center justify-center "
               >
                 <div className="relative flex w-14 h-14  items-center justify-center">
                   <SkillIcon className="w-14 h-14" />
                 </div>
-                <p className="mt-4">{item?.name}</p>
+                <p className="mt-4">{item.name}</p>
               </div>
             )
           })}
@@ -41,3 +41,4 @@ function SkillSection(props: Props) {
 }
 
 export default SkillSection
+
