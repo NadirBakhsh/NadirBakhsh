97522b340c3ae394a4d3e6ff40cc423820eb9114
NadirBakhsh <nadir4k2010@gmail.com>
2025-02-24 20:18:59 +0500

    Fixed responsive

--- app/components/sections/SkillSection.tsx
+++ app/components/sections/SkillSection.tsx
@@ -11,14 +11,14 @@ function SkillSection(props: Props) {
   } = portfolioJson
 
   return (
-    <div className="skills h-auto bg-white w-full py-10  mt-16">
+    <div className="skills h-auto bg-white w-full py-10  mt-16 px-5">
       <div className="container mx-auto flex flex-col items-center mt-10">
         <h2 className="text-5xl text-center">
           {" "}
           My <span className="font-extrabold">{title}</span>
         </h2>
 
-        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-5 gap-6 md:gap-10 my-12">
+        <div className="grid grid-cols-2 sm:grid-cols-3  lg:grid-cols-5 gap-6 md:gap-10 my-12">
           {skills.map((item) => {
             const SkillIcon = Icons[item.icon as keyof typeof Icons]
 
