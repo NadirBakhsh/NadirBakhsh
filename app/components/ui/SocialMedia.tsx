abfe9db0936402e14068f9f74ae0fa126516515d
NadirBakhsh <nadir4k2010@gmail.com>
2025-02-24 20:00:01 +0500

    soical icons added

--- app/components/ui/SocialMedia.tsx
+++ app/components/ui/SocialMedia.tsx
@@ -1,45 +1,30 @@
-import { Link } from "@remix-run/react";
-import { Icons } from "~/components/icon";
+import { Link } from "@remix-run/react"
+import { Icons } from "~/components/icon"
+import { SocialMediaLinks } from "~/json/portfolio.json"
 
 interface Props {
-  size?: "sm" | "lg";
+  size?: "sm" | "lg"
 }
 
 function SocialMedia(props: Props) {
-  const { size = "lg" } = props;
+  const { size = "lg" } = props
 
-  const className =
-    size === "sm" ? "social-media-link-sm" : "social-media-link";
+  const className = size === "sm" ? "social-media-link-sm" : "social-media-link"
 
   return (
     <>
-      <Link to={""}>
-        <div className={className}>
-          <Icons.Facebook />
-        </div>
-      </Link>
-      <Link to={""}>
-        <div className={className}>
-          <Icons.Instagram />
-        </div>
-      </Link>
-      <Link to={""}>
-        <div className={className}>
-          <Icons.LinkedIn />
-        </div>
-      </Link>
-      <Link to={""}>
-        <div className={className}>
-          <Icons.Github />
-        </div>
-      </Link>
-      <Link to={""}>
-        <div className={className}>
-          <Icons.RedRabbit />
-        </div>
-      </Link>
+      {SocialMediaLinks.map(({ url, icon, title }, index) => {
+        const SoicalMeadiaIcon = Icons[icon as keyof typeof Icons]
+        return (
+          <Link title={title} target="_blank" key={url + index} to={url}>
+            <div className={className}>
+              <SoicalMeadiaIcon  />
+            </div>
+          </Link>
+        )
+      })}
     </>
-  );
+  )
 }
 
-export default SocialMedia;
+export default SocialMedia
