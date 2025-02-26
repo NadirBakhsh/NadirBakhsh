a311db4249b9ace4b4685b50b23ef9d38c59cd27
NadirBakhsh <nadir4k2010@gmail.com>
2025-02-26 20:29:12 +0500

    TestimonialSection

--- app/components/sections/TestimonialSection.tsx
+++ app/components/sections/TestimonialSection.tsx
@@ -1,25 +1,27 @@
-import { TestimonialCarousel } from "../ui/TestimonialCarousel";
-import portfolioJson from '~/json/portfolio'
+import portfolioJson from "~/json/portfolio.json"
+import { TestimonialCarousel } from "../ui/TestimonialCarousel"
 
 interface Props {}
 
 function TestimonialSection(props: Props) {
-  const {} = props;
-  const { TestimonialSection } = portfolioJson
+  const {} = props
+  const {
+    Testimonials: { title }
+  } = portfolioJson
 
   return (
     <div className="Testimonial h-auto bg-white px-5">
       <div className="container mx-auto my-24 py-1">
         <h2 className="text-4xl sm:text-5xl text-center text-black ">
           {" "}
-          My <span className="font-extrabold text-center">Testimonial</span>
+          My <span className="font-extrabold text-center">{title}</span>
         </h2>
-        <div className="container relative mx-auto h-[400px] mt-10 md:mt-20">
+        <div className="container relative mx-auto  mt-10 md:mt-20">
           <TestimonialCarousel />
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default TestimonialSection;
+export default TestimonialSection
--- app/components/ui/TestimonialCarousel.tsx
+++ app/components/ui/TestimonialCarousel.tsx
@@ -1,90 +1,48 @@
-import { useEffect, useState } from "react"
 import {
   Carousel,
-  CarouselApi,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious
 } from "~/components/ui/carousel"
-import { cn } from "~/lib/utils"
+import { Testimonials } from "~/json/portfolio.json"
 
 export function TestimonialCarousel() {
-  const [api, setApi] = useState<CarouselApi>()
-  const [current, setCurrent] = useState(0)
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    if (!api) {
-      return
-    }
-
-    setCount(api.scrollSnapList().length)
-    setCurrent(api.selectedScrollSnap() + 1)
-
-    api.on("select", () => {
-      setCurrent(api.selectedScrollSnap() + 1)
-    })
-
-    if (count > 3) {
-      const interval = setInterval(() => {
-        api?.scrollNext()
-      }, 3000)
-      return () => clearInterval(interval)
-    }
-  }, [api, count])
+  const { reviews } = Testimonials
 
   return (
     <div className="relative h-auto">
       <Carousel
-        setApi={setApi}
         opts={{
-          align: "start"
+          loop: true,
+          align: "start",
+          dragFree: false,
+          slidesToScroll: 1
         }}
-        className="w-full"
       >
-        <CarouselContent className="justify-center">
-          {Array.from({ length: 3 }).map((_, index) => (
-            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-              <div
-                key={index}
-                className="flex p-10 relative border group hover:bg-black items-center flex-col text-base rounded-2xl bg-white  shadow-md transition duration-300 h-[400px]"
-              >
-                <div className="avatar h-20 w-20 border-2 border-black group-hover:border-white rounded-full"></div>
+        <CarouselContent>
+          {reviews.map((review, index) => (
+            <CarouselItem key={index} className="md:basis-1/1 lg:basis-1/3">
+              <div className="flex p-10 relative border group hover:bg-black items-center flex-col text-base rounded-2xl bg-white  shadow-md transition duration-300 h-[400px]">
+                <div className="avatar h-20 w-20 border-2 overflow-clip border-black group-hover:border-white rounded-full">
+                  <img src={review?.user_image_url} />
+                </div>
                 <h5 className="text-lg text-black mt-4 group-hover:text-white font-semibold underline underline-offset-4">
-                  Flora sheen
+                  {review?.username}
                 </h5>
                 <p className="text-sm text-primary-neutral pt-8 pb-4 font-light text-center  group-hover:text-white ">
-                  I recently had to jump on 10+ different calls across eight
-                  different countries to find the right owner.
+                  {review?.comment}
                 </p>
                 <p className="text-sm absolute bottom-10 text-black group-hover:text-white">
-                  Designer
+                  From {review?.reviewer_country}
                 </p>
               </div>
             </CarouselItem>
           ))}
         </CarouselContent>
-        {count > 1 && (
-          <>
-            <CarouselPrevious />
-            <CarouselNext />
-          </>
-        )}
+        <CarouselPrevious />
+        <CarouselNext />
       </Carousel>
-      {count > 1 && (
-        <div className="flex justify-center mt-10 gap-1">
-          {Array.from({ length: count }).map((_, index) => (
-            <div
-              key={index}
-              className={cn(
-                "h-3 w-3 bg-white border border-black rounded-full transition duration-300",
-                index === current - 1 ? "bg-black" : "bg-transparent"
-              )}
-            />
-          ))}
-        </div>
-      )}
     </div>
   )
 }
